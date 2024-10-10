cac23c8089566123385259224a6af95bdfa9b69f
Elendu Franklin <elendufranklin66@gmail.com>
2024-10-10 01:33:23 +0100

    bug fix

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,6 @@
 'use client'
+import React, { useState } from 'react';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import DesktopNavLinks from './NavComponents/DesktopNavLinks';
