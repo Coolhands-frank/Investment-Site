ed523f897feabc5d257ae1ce0f310714b743c83b
Elendu Franklin <elendufranklin66@gmail.com>
2025-02-02 17:22:46 +0100

    font update

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -3,7 +3,7 @@ import SideBar from "../../components/DashboardComponents/Sidebar"
 
 export default function DashboardLayout({ children }) {
   return (
-      <main className={`${poppins.className}antialiased px-4 md:px-0 py-8 bg-orange-500 flex flex-col md:flex-row h-screen`}>
+      <main className={`${poppins.className} antialiased px-4 md:px-0 py-8 bg-orange-500 flex flex-col md:flex-row h-screen`}>
         <SideBar />
         <div className="drop-shadow-xl md:ml-0 px-4 md:px-8 md:mr-4 lg:mr-8 rounded-xl w-full h-full bg-gray-100">
             {children}
