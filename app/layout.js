b792ac0e6489ae8c0103d40ca648287f1a3bd658
Elendu Franklin <elendufranklin66@gmail.com>
2025-02-03 18:08:43 +0100

    metadata

--- app/layout.js
+++ app/layout.js
@@ -12,10 +12,6 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-        <head>
-          <meta charSet="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-        </head>
         <body className={asap.className}>
         {/*  {!noNavRoutes.includes(pathname) && <Navbar />} */}
             <Navbar />
