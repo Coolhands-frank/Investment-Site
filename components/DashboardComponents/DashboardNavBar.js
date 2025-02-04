44c9367f571c8ce0e875a323c8293e7328aa7f3e
Elendu Franklin <elendufranklin66@gmail.com>
2025-02-04 09:23:49 +0100

    mobile layout

--- components/DashboardComponents/DashboardNavBar.js
+++ components/DashboardComponents/DashboardNavBar.js
@@ -2,16 +2,15 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import SideBar from './Sidebar';
-import MobileDrawer from '../NavComponents/MobileDrawer';
-import Image from 'next/image';
+import ProfileDrawer from './ProfileDrawer';
 import { CogIcon } from '@heroicons/react/24/solid';
 
 const DashboardNavBar = () => {
 
-  const [isToggleDrawer, setIsToggleDrawer] = useState(false)
+  const [isProfileDrawer, setIsProfileDrawer] = useState(false)
   
-  const handleDrawerToggle = () => {
-    setIsToggleDrawer(!isToggleDrawer);
+  const handleProfileToggle = () => {
+    setIsProfileDrawer(!isProfileDrawer);
   };
 
   return (
@@ -21,13 +20,13 @@ const DashboardNavBar = () => {
       </div>
 
       <div className="md:hidden bg-orange-500 py-4 px-8 flex flex-row justify-between items-center">
-        <button className="border rounded-full w-8 h-8 bg-gray-100" onClick={handleDrawerToggle}>
+        <button className="border rounded-full w-8 h-8 bg-gray-100" onClick={handleProfileToggle}>
           
         </button>
         <Link className="rounded-full text-white hover:bg-gray-700 transition duration-200" href="#">
             <CogIcon className="h-8 w-8" />
         </Link>
-        <MobileDrawer isOpen={isToggleDrawer} onClose={handleDrawerToggle} />
+        <ProfileDrawer isOpen={isProfileDrawer} onClose={handleProfileToggle} />
     </div>
     </>
   );
--- components/DashboardComponents/ProfileDrawer.js
+++ components/DashboardComponents/ProfileDrawer.js
@@ -1,77 +1,89 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import Profile from './UserProfile'
+import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import Profile from './UserProfile';
+import Image from 'next/image';
 
-export default function SideBar() {
-
+export default function ProfileDrawer({ isOpen, onClose }) {
     return (
-        <div className="flex flex-col items-center justify-center px-4">
-            <h1 className="font-black text-lg lg:text-xl text-orange-100">Krane Finance</h1>
+      <div
+        className={`fixed flex flex-col z-10 top-0 right-full h-full w-1/2 bg-orange-500 text-black transition-transform duration-300 transform ${
+          isOpen ? 'translate-x-0' : 'translate-x-full'
+        }`}
+      >
+        {/*
+        <div className="flex px-12 py-2 justify-between items-center">
+            <button className="p-3 text-gray-100" onClick={onClose}>
+                <FontAwesomeIcon className="text-xl" icon={faXmark} />
+            </button>
+        </div>
+        */}
+        <div className="h-full px-8 flex flex-col justify-center items-center">
             <Profile />
-            <div className="border-t-2 pt-4 px-4 md:px-10">
-                <ul className="text-xs lg:text-base space-y-2">
-                    <li className="">
+            <div className="border-t-2 pt-6 mt-2 px-4 text-gray-100">
+                <ul className="text-xs space-y-4">
+                    <li className="" onClick={onClose}>
                         <Link className="flex items-center" href="/dashboard">
                             <Image
                                 src={"/images/Monitor.png"}
                                 width={16}
                                 height={16}
-                                alt=""
+                                alt="overview icon"
                                 className="mr-2" 
                             />
                             <p>Overview</p>
                         </Link>
                     </li>
 
-                    <li className="">
-                        <Link className="flex items-center" href="/campaign">
+                    <li className="" onClick={onClose}>
+                        <Link className="flex items-center" href="#">
                             <Image
                                 src={"/images/dialing-numbers.png"}
                                 width={16}
                                 height={16}
-                                alt=""
+                                alt="withdraw icon"
                                 className="mr-2" 
                             />
                             <p>Withdraw</p>
                         </Link>
                     </li> 
-                    <li className="">
+                    <li className="" onClick={onClose}>
                         <Link className="flex items-center" href="#">
                             <Image
                                 src={"/images/add-dollar.png"}
                                 width={16}
                                 height={16}
-                                alt=""
+                                alt="invest icon"
                                 className="mr-2" 
                             />
                             <p>Invest Funds</p>
                         </Link>
                     </li>
-                    <li>
+                    <li onClick={onClose}>
                         <Link className="flex items-center" href="#">
                             <Image
                                 src={"/images/shopping-cart.png"}
                                 width={16}
                                 height={16}
-                                alt="" 
+                                alt="products image" 
                                 className="mr-2"
                             />
                            <p>Products</p>
                         </Link>
                     </li>
-                    <li>
+                    <li onClick={onClose}>
                         <Link className="flex items-center" href="#">
                             <Image
                                 src={"/images/health-graph.png"}
                                 width={16}
                                 height={16}
-                                alt="" 
+                                alt="history image" 
                                 className="mr-2"
                             />
                             <p>History</p>
                         </Link>
                     </li>
-                    <li>
+                    <li onClick={onClose}>
                         <Link className="flex items-center" href="#">
                             <Image
                                 src={"/images/Settings.png"}
@@ -85,17 +97,20 @@ export default function SideBar() {
                     </li>
                 </ul>
 
-                <Link className="text-xs lg:text-base flex items-center mt-8" href="#">
+                <Link className="text-xs flex items-center mt-10" href="#" onClick={onClose}>
                     <Image
                         src={"/images/logout-rounded.png"}
                         width={16}
                         height={16}
-                        alt="" 
+                        alt="Logout Image" 
                         className="mr-2"
                     />
                     <p>Log Out</p>
                 </Link>
             </div>   
         </div>
-    )
+        
+        
+      </div>
+    );
 }
