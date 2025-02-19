ed5265fe77ce3f24db34dd10cc156d8d7da298a0
Elendu Franklin <elendufranklin66@gmail.com>
2025-02-19 06:36:55 +0100

    supabase url bug fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    swcMinify: true, // Enable SWC minification for better performance
+    compiler: {
+      removeConsole: process.env.NODE_ENV === "production", // Remove console logs in production
+    },
     env: {
       NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
       NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
